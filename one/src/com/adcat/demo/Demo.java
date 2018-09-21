d10bbff884d32e2c43961da0b8bb646e9bb994b1
AodaCat <pczqiang@gmail.com>
2018-09-21 16:49:09 +0800

    Initial commit

---
+++ one/src/com/adcat/demo/Demo.java
@@ -0,0 +1,25 @@
+package com.adcat.demo;
+
+public class Demo {
+
+    public static void main(String[] args){
+
+
+
+
+
+
+//        // 构造器和成员方法重载
+//        Tree tree = new Tree();
+//        tree.print();
+//        tree.print("print方法重载");
+//
+//        Tree tree1 = new Tree(3);
+//        tree1.print();
+//        tree1.print("print方法重载");
+//
+//        //this关键字的使用
+//        Leaf x = new Leaf();
+//        x.increment().increment().increment().increment().print();
+    }
+}
---
+++ one/src/com/adcat/demo/Tree.java
@@ -0,0 +1,22 @@
+package com.adcat.demo;
+
+public class Tree {
+    int number;
+
+    Tree(){
+        System.out.println("无参构造方法");
+    }
+
+    Tree(int number){
+        this.number = number;
+        System.out.println("有参构造方法");
+    }
+
+    void print(){
+        System.out.println("print:number = " + number);
+    }
+
+    void print(String s){
+        System.out.println(s + "print:number = " + number);
+    }
+}
